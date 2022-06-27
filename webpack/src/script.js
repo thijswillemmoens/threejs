4a14d95fc769d407eb94b0dcf25275108e318691
Thijs Moens <post@thijs.email>
2022-06-27 09:21:33 +0200

    👌 IMPROVE: Add an Axe helper

--- webpack/src/script.js
+++ webpack/src/script.js
@@ -8,7 +8,21 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial( { color: '#a08f68' } );
 const mesh = new THREE.Mesh( geometry, material );
+
+// Give the Mesh a starting position
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+
+// You can set the starting position with set
+mesh.position.set(0.7, -0.6, 1);
+
 scene.add( mesh );
+
+// Add an Axehelper for better 3D modeling
+const axesHelper = new THREE.AxesHelper(2); // parameter is the size of the helper
+scene.add( axesHelper );
+
 
 // Sizes for aspect ratio
 const sizes = {
